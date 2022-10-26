5bb8e597be41b8bade2ea94a62d7266ef517e4a7
ja.pardor1 <ja.pardor1@uniandes.edu.co>
2022-10-26 15:24:21 -0500

    creacion de las pruebas para el componente book list

--- src/app/book/book.service.spec.ts
+++ src/app/book/book.service.spec.ts
@@ -1,16 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed  } from '@angular/core/testing';
 
 import { BookService } from './book.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('BookService', () => {
   let service: BookService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BookService]
+    });
     service = TestBed.inject(BookService);
   });
 
-  it('should be created', () => {
+  it('should ...', inject([BookService], (service: BookService) => {
     expect(service).toBeTruthy();
-  });
+  }));
 });
